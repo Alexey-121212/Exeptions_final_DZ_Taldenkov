6172cc8ff79995de8245c30d929681fc37a71088
Alexey <alexmail121212@gmail.com>
2024-04-14 02:19:20 +0300

    Outputer done

--- HumanCard.java
+++ HumanCard.java
@@ -17,40 +17,20 @@ public class HumanCard {
         this.firstName = firstName;
     }
 
-    public String getSecondName() {
-        return secondName;
-    }
-
     public void setSecondName(String secondName) {
         this.secondName = secondName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
     }
 
     public void setMiddleName(String middleName) {
         this.middleName = middleName;
     }
 
-    public String getBirthDate() {
-        return birthDate;
-    }
-
     public void setBirthDate(String birthDate) {
         this.birthDate = birthDate;
     }
 
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-
-    public Sex getSex() {
-        return sex;
     }
 
     public void setSex(Sex sex) {
--- MyExeptions/DirectoryWithNameExists.java
+++ MyExeptions/DirectoryWithNameExists.java
@@ -1,7 +1,5 @@
 package MyExeptions;
 
-import java.io.IOException;
-
 public class DirectoryWithNameExists extends RuntimeException {
     public DirectoryWithNameExists() {
         super("Есть директория с таким именем, удалите ее");
